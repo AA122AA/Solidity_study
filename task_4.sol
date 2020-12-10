cc9d0fb3f2e59775f80e476f13a030e96eb18a18
Gromov Artem <gromov@pop-os.localdomain>
2020-12-10 19:39:33 +0300

    add req for address

--- task_4.sol
+++ task_4.sol
@@ -4,10 +4,12 @@ contract task_4 {
     mapping (address=>mapping(uint=>bool)) deposit_type;
 
     function addDepositType(address _user, uint _type, bool _premition ) external {
+        require(_user != address(0));
         deposit_type[_user][_type]=_premition;
     }
 
     function getDepositType(address _user, uint _type) external view returns(bool) {
+        require(_user != address(0));
         return deposit_type[_user][_type];
     }
 
