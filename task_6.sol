4494f158f21748927829da8f6f0c44cc0390c7f0
AA122AA <gromartem2000@gmail.com>
2020-12-29 20:57:26 +0300

    add some fixes

--- task_6.sol
+++ task_6.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.6.12;
 
-contract task_6 {
+contract Task6 {
     mapping (address=>uint256) userDeposit; 
     address payable private owner;
 
@@ -25,7 +25,9 @@ contract task_6 {
         _;
     }
 
-    function unitType(string memory _unit, uint256 _deposit) internal pure returns(uint256) {
+    // Данная функция нужна для того, чтобы переводить введенный депозит в wei так как все расчеты производятся в них
+    // Я решил осатвить такое решение, дабы не полагаться на сторонний софт
+    function _unitType(string memory _unit, uint256 _deposit) internal pure returns(uint256) {
         uint256 conv_dep;
         if (keccak256(bytes(_unit)) == keccak256("w")) {
             conv_dep = _deposit;
@@ -42,30 +44,38 @@ contract task_6 {
         return conv_dep;
     }
 
+    function contractBalance() public view returns(uint256) {
+        return address(this).balance;
+    }
+
     function store() external payable{
         require(msg.value > 0);
         userDeposit[msg.sender] = userDeposit[msg.sender] + msg.value;
     }
 
     function ownerReceive(uint256 _deposit, string memory _unit) unitCheck(_unit) external onlyOwner{
-        uint256 conv_dep = unitType(_unit, _deposit);
+        uint256 conv_dep = _unitType(_unit, _deposit);
         require(conv_dep <= userDeposit[owner]);
+        require(contractBalance() >= conv_dep);
         owner.transfer(conv_dep);
         userDeposit[owner] = userDeposit[owner] - conv_dep;
     }
     
     function userReceive(uint256 _deposit, string memory _unit) unitCheck(_unit) external {
-        uint256 conv_dep = unitType(_unit, _deposit);
+        uint256 conv_dep = _unitType(_unit, _deposit);
         require(conv_dep <= userDeposit[msg.sender]);
-        msg.sender.send(conv_dep); //ругается на то, что send() возвращает не ошибку, а false
-        userDeposit[msg.sender] = userDeposit[msg.sender] - conv_dep;
+        require(contractBalance() >= conv_dep);
+        bool isSend = msg.sender.send(conv_dep); //ругается на то, что send() возвращает не ошибку, а false
+        if (isSend) {
+            userDeposit[msg.sender] = userDeposit[msg.sender] - conv_dep;
+        } 
     }
 
     function getUserDeposit(address _user) view external returns(uint256){
        return userDeposit[_user]; // тут решил показывать в веях, хотя можно реализовать такую же схему, как и в Recive
     }
 
-    function getYourDeposit() view external returns(uint256){
+    function getYourDeposit() external view returns(uint256){
        return userDeposit[msg.sender]; // тут решил показывать в веях, хотя можно реализовать такую же схему, как и в Recive
     }
 }
